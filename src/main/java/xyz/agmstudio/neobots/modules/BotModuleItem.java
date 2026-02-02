18533c73137231ab6cfef5e1e91f974b4fdb5241
Ashenguard <myminecraft2019@gmail.com>
2026-02-02 15:33:25 +0330

    Added Movement Module

---
+++ src/main/java/xyz/agmstudio/neobots/modules/BotModuleItem.java
@@ -0,0 +1,9 @@
+package xyz.agmstudio.neobots.modules;
+
+import net.minecraft.world.item.Item;
+
+public abstract class BotModuleItem extends Item {
+    public BotModuleItem(Properties properties) {
+        super(properties);
+    }
+}
