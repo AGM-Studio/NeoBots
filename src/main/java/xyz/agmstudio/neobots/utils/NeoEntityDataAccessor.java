91da48c9f8449ddf456087c6d342eac702767c58
Ashenguard <myminecraft2019@gmail.com>
2026-02-08 23:45:04 +0330

    Fix multiple bot interaction with synced data

--- src/main/java/xyz/agmstudio/neobots/utils/NeoEntityDataAccessor.java
+++ src/main/java/xyz/agmstudio/neobots/utils/NeoEntityDataAccessor.java
@@ -14,18 +14,16 @@ import java.util.Objects;
 @ParametersAreNonnullByDefault
 public class NeoEntityDataAccessor<T> {
     private final EntityDataAccessor<T> accessor;
-    private T lastValue;
     public NeoEntityDataAccessor(Class<? extends SyncedDataHolder> clazz, EntityDataSerializer<T> serializer) {
         this.accessor = SynchedEntityData.defineId(clazz, serializer);
     }
     public void build(SynchedEntityData.@NotNull Builder builder, T def) {
         builder.define(accessor, def);
-        this.lastValue = def;
     }
     public void set(Entity entity, T value) {
-        if (Objects.equals(lastValue, value)) return;
+        T current = entity.getEntityData().get(accessor);
+        if (Objects.equals(current, value)) return;
         entity.getEntityData().set(accessor, value);
-        this.lastValue = value;
     }
     public T get(Entity entity) {
         return entity.getEntityData().get(accessor);
